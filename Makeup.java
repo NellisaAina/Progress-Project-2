fbbd84fa8fd66bfd45a382b51909b1f191d27a33
NellisaAina <73329128+NellisaAina@users.noreply.github.com>
2021-01-17 21:56:25 +0800

    Add files via upload

---
+++ Makeup.java
@@ -0,0 +1,17 @@
+
+public class Makeup {
+	String typeOfMakeup;
+	String brand;
+	String howToUse;
+	String whereToBuy;
+    
+    public Makeup(String t,String b, String u, String w)
+    {
+    	this.typeOfMakeup = t;
+    	this.brand = b;
+    	this.howToUse = u;
+    	this.whereToBuy = w;
+
+    }
+
+}
